e2133cec8d303db3f4f9ae5e3566e179e498ca25
Michael Jordan <mjthecoder53161@gmail.com>
2022-02-14 15:38:20 +0900

    Updated main module

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/")
+async def vegan_home():
+    return {"message": "A healthy inside starts from inside"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
